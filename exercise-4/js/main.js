0d01812948a78a8b740ec13b69abddc00afc5b3f
mkfreeman <michael.k.freeman@gmail.com>
2016-03-18 15:56:19 -0700

    Finished exercise 4

--- exercise-4/js/main.js
+++ exercise-4/js/main.js
@@ -1,12 +1,16 @@
 // Main JavaScript File
 
-// You'll have to wait for you page to load to assign events to the elements created in your index.html File
-document.addEventListener("DOMContentLoaded", function(event) {
-  // Animate your rectangle to these new set of properties:
-      // height of 300
-      // width of 500
-      // opacity of .5
+// You'll have to wait for you page to load to assign events to the elements created in your index.html file
+$(function() {
+  // Animate your div to these new set of properties:
 
-  // When the transition finishes, fade out your paragraphs and your circle.
-
+  $('#my-div').animate({
+    opacity:.5, // opacity of .5
+    width:500, // width of 500
+    height:300 // height of 300
+  }, 2000, function() {
+    // When the transition finishes, fade out your paragraphs and your circle.
+     // Select all paragraphs and fade them out
+    $('p, circle').fadeOut(500)
+  })
 });
