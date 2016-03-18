114ec657218a520de52084225000388896fff607
mkfreeman <michael.k.freeman@gmail.com>
2016-03-18 15:39:20 -0700

    Answered exercise 1

--- exercise-1/js/main.js
+++ exercise-1/js/main.js
@@ -1,29 +1,33 @@
 // Main JavaScript File
 
-// You'll have to wait for you page to load to assign events to the elements created in your index.html File
-document.addEventListener("DOMContentLoaded", function(event) {
-  // Select elements with class "italics" and set their css "font-style", to "italic"
+// Defining the changeText variable so that it's globally available for assignment.
+var changeText;
 
+// You'll have to wait for you page to load to assign events to the elements created in your index.html File
+$(function(){
+  // Select elements with class "italics" and set their css "font-style", to "italic"
+  $('.italics').css('font-style', 'italic');
 
   // Select all svgs and change their attr "height" to 200
-
+  $('svg').attr('height', 200);
 
   // Select your first svg and change it's width attribute to 10
-
+  $('svg:first').attr("width", 10);
 
   // Select even rows in the table and set their css "background" to "lightgray"
-
+  $("tr:even").css('background', 'lightgray');
 
   // Select the input and set the attribute "placeholder" to "start typing"
-
+  $('input').attr('placeholder', 'start typing');
 
   // Set the input val to "hello"
-
-
+  $('input').val('hellos');
 
   // Fill in this function that selects the text with id "change" and sets it text to the value in the text box.  Click "submit" to see if it worked.
-  var changeText = function() {
+  changeText = function() {
+    var text = $('input').val()
+    $("#change").text(text)
+    $('input').val('')
+  };
 
-
-  }
 });
