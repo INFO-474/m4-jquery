603c65f4eb67665eb9488321b652fed65971571a
mkfreeman <michael.k.freeman@gmail.com>
2016-03-18 15:46:02 -0700

    Using jQuery for document ready

--- exercise-3/js/main.js
+++ exercise-3/js/main.js
@@ -1,7 +1,7 @@
 // Main JavaScript File
 
 // You'll have to wait for you page to load to assign events to the elements created in your index.html File
-document.addEventListener("DOMContentLoaded", function(event) {
+$(function() {
   // Select all <p> elements and assign a click event that removes the clicked element (recall `this`)
   // Select the <circle>, and when it's clicked, turn it red
   // Each time the <circle> is clicked, change its radius from 10 to 40
--- exercise-4/js/main.js
+++ exercise-4/js/main.js
@@ -1,7 +1,7 @@
 // Main JavaScript File
 
 // You'll have to wait for you page to load to assign events to the elements created in your index.html File
-document.addEventListener("DOMContentLoaded", function(event) {
+$(function() {
   // Animate your rectangle to these new set of properties:
       // height of 300
       // width of 500
